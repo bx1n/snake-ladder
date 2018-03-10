8dc1de765fd94fb30e7f947dd672c7c4c676bfb1
abhinand4858 <abhinand4858@gmail.com>
2018-03-10 14:11:08 +0530

    Added computation

--- client.py
+++ client.py
@@ -1,9 +1,20 @@
 import socket
 
-soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+BUFFER_SIZE = 1024
+MESSAGE = '{"player_count":3,"board_dimension":6,"ladders":{"19":24,"27":34,"28":31,"32":33},"snakes":{"6":5,"11":8,"30":29,"35":17},"die_tosses":{"1":{"1":5,"2":1,"3":5},"2":{"1":5,"2":1,"3":6},"3":{"1":5,"2":3,"3":3},"4":{"1":4,"2":1,"3":5},"5":{"1":1,"2":3,"3":3},"6":{"1":6,"2":6,"3":6},"7":{"1":3,"2":6,"3":3},"8":{"1":5,"2":3,"3":4},"9":{"1":1,"2":4,"3":3},"10":{"1":1,"2":2}}}\n'
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 ip = socket.gethostname()
 port = 8080
+s.connect((ip, port))
 
-soc.connect((ip, port))
-soc.send('{"player_count":3,"board_dimension":6,"ladders":{"19":24,"27":34,"28":31,"32":33},"snakes":{"6":5,"11":8,"30":29,"35":17},"die_tosses":{"1":{"1":5,"2":1,"3":5},"2":{"1":5,"2":1,"3":6}}}\n')
-soc.close()
+s.send(MESSAGE)
+
+data = s.recv(BUFFER_SIZE)
+print data
+
+
+data = s.recv(BUFFER_SIZE)
+print data
+
+s.close()
--- server.py
+++ server.py
@@ -1,16 +1,17 @@
 import socket
 import json
 
-soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 ip = socket.gethostname()
 port = 8080
-soc.bind((ip, port))
+s.bind((ip, port))
 
-soc.listen(5)
+s.listen(5)
 
-while True:
-    con, ip = soc.accept()
+
+while 1:
+    con, ip = s.accept()
     data = con.recv(1024)
 
     data = json.loads(data)
@@ -21,18 +22,28 @@ while True:
 
     cnt = data['player_count']
     pos = {}
+
     #dic to map position of user
     for i in range(1,cnt+1):
         pos[i] = 0
 
+    ladd = {int(k): int(v) for k, v in ladd.items()}
+    snake = {int(k): int(v) for k, v in snake.items()}
 
     for key, value in data['die_tosses'].iteritems():
         for k, v in value.iteritems():
-            pos[int(k)]=int(pos[int(k)])+int(v)
+            k=int(k)
+            pos[k] = int(pos[k]) + int(v)
+            if ladd.get(pos[k]):
+                con.send('Player ' + str(k) + " got a ladder at " + str(pos[k]) + ' to ' + str(ladd.get(pos[k])) + '\n')
+                pos[k] = ladd.get(pos[k])
 
-    print pos
-    # for i,j in pos:
-    #     print i, j
+            if snake.get(pos[k]):
+                con.send('Player ' + str(k) + " got a snake at " + str(pos[k]) + ' to ' + str(snake.get(pos[k])) + '\n')
+                pos[k] = snake.get(pos[k])
+
+            if pos[k] == 100:
+                print 'Player '+str(k)+' won'
 
 
     con.close()
